8db98e60ca3f2bd9931c0b8fe4f2ad23d848a30e
Fabio Ivona <fabio.ivona@defstudio.it>
2021-10-12 16:48:42 +0200

    implements toBeRedirect() expectation

--- src/Expectations/Response.php
+++ src/Expectations/Response.php
@@ -4,15 +4,28 @@ declare(strict_types=1);
 
 use Illuminate\Testing\TestResponse;
 use Pest\Expectation;
+use PHPUnit\Framework\ExpectationFailedException;
 
 expect()->extend(
     'toBeRedirect',
-
+    /**
+     * Assert that the given response is a redirection.
+     */
     function (string $uri = null): Expectation {
         /** @var TestResponse $response */
         $response = $this->value;
 
-        $response->assertRedirect($uri);
+        $response->assertRedirect();
+
+        if ($uri === null) {
+            return $this;
+        }
+
+        try {
+            $response->assertLocation($uri);
+        } catch (ExpectationFailedException $exception) {
+            throw new ExpectationFailedException("Failed asserting that the redirect uri [{$response->headers->get('Location')}] matches [$uri]");
+        }
 
         return $this;
     }
--- tests/Expect/Response/toBeRedirect.php
+++ tests/Expect/Response/toBeRedirect.php
@@ -1,9 +1,40 @@
 <?php
 
 use function Pest\Laravel\get;
+use PHPUnit\Framework\ExpectationFailedException;
 
 test('pass', function () {
     $response = get('/redirect');
 
-    expect($response)->toBeRedirect('/login');
+    expect($response)->toBeRedirect();
 });
+
+test('pass with uri check', function () {
+    $response = get('/redirect');
+
+    expect($response)->toBeRedirect('/ok');
+});
+
+test('fails', function () {
+    $response = get('/ok');
+
+    expect($response)->toBeRedirect();
+})->throws(ExpectationFailedException::class, 'Response status code [200] is not a redirect status code');
+
+test('fails with uri check', function () {
+    $response = get('/redirect/out');
+
+    expect($response)->toBeRedirect('/ok');
+})->throws(ExpectationFailedException::class, 'Failed asserting that the redirect uri [https://www.google.it] matches [/ok]');
+
+test('pass with negation', function () {
+    $response = get('/ok');
+
+    expect($response)->not->toBeRedirect();
+});
+
+test('fails with negation', function () {
+    $response = get('/redirect');
+
+    expect($response)->not->toBeRedirect();
+})->throws(ExpectationFailedException::class, "Expecting Illuminate\Testing\TestResponse Object (...) not to be redirect");
