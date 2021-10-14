6429d14e7c6ea6f078bb554aa610221a71b4a09f
David Adi Nugroho <davidadi216@gmail.com>
2021-10-14 23:38:39 +0700

    adds toHaveValid and toHaveInvalid expectation
    Co-authored-by: Fabio Ivona <fabio.ivona@defstudio.it>

---
+++ tests/Expect/Response/toHaveInvalid.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\App;
+use function Pest\Laravel\post;
+use PHPUnit\Framework\ExpectationFailedException;
+
+test('pass', function () {
+    $response = post('/validate', ['email' => 'taylor']);
+
+    if (version_compare(App::version(), '8.55', '>=')) {
+        expect($response)->toHaveInvalid(['email']);
+    }
+
+    expect($response)->toBeRedirect();
+});
+
+test('fails', function () {
+    $response = post('/validate', ['email' => 'taylor@laravel.com']);
+
+    if (version_compare(App::version(), '8.55', '<')) {
+        throw new ExpectationFailedException('Session is missing expected key [errors]');
+    }
+
+    expect($response)->toHaveInvalid(['email']);
+})->throws(ExpectationFailedException::class, 'Session is missing expected key [errors]');
+
+test('pass with negation', function () {
+    $response = post('/validate', ['email' => 'taylor@laravel.com']);
+
+    if (version_compare(App::version(), '8.55', '>=')) {
+        expect($response)->not->toHaveInvalid(['email']);
+    }
+
+    expect($response)->not->toBeRedirect();
+});
+
+test('fails with negation', function () {
+    $response = post('/validate');
+
+    if (version_compare(App::version(), '8.55', '<')) {
+        throw new ExpectationFailedException('Expecting Illuminate\Testing\TestResponse Object (...) not to have invalid Array (...)');
+    }
+
+    expect($response)->not->toHaveInvalid(['email']);
+})->throws(ExpectationFailedException::class, 'Expecting Illuminate\Testing\TestResponse Object (...) not to have invalid Array (...)');
