bad472a7da3a38062797a143ff2b0630a0993be5
Fabio Ivona <fabio.ivona@defstudio.it>
2021-10-21 19:58:20 +0200

    fix tests

--- tests/Expect/Time/toBeFuture.php
+++ tests/Expect/Time/toBeFuture.php
@@ -8,7 +8,7 @@ test('pass', function () {
 
 test('fails', function () {
     expect('1950-01-01')->toBeFuture();
-})->throws(ExpectationFailedException::class, 'Failed to assert that [2999-01-01 00:00:00] is in the future');
+})->throws(ExpectationFailedException::class, 'Failed to assert that [1950-01-01 00:00:00] is in the future');
 
 test('pass negated', function () {
     expect(now())->not->toBeFuture();
