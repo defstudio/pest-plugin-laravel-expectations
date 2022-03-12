87481711bd7f775b23e2cdb29e2edb9f076e2d3c
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-12 12:02:57 +0100

    add analytics

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,14 +1,17 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
-  docs: {
-    primaryColor: '#1093ff'
-  },
-  target: 'static',
-  content: {
-    liveEdit: false
-  },
-  router: {
-    base: '/pest-plugin-laravel-expectations'
-  }
+    docs: {
+        primaryColor: '#1093ff'
+    },
+    target: 'static',
+    content: {
+        liveEdit: false
+    },
+    router: {
+        base: '/pest-plugin-laravel-expectations'
+    },
+    buildModules: [
+        ['@nuxtjs/google-analytics', {id: 'UA-211287441-2'}]
+    ]
 })
