f5fc2b53a15e830b806fc9a9c13079511544bc26
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-12 11:42:02 +0100

    larastan

--- src/Expectations/Database.php
+++ src/Expectations/Database.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace DefStudio\PestLaravelExpectations;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Str;
 use Pest\Expectation;
 use function Pest\Laravel\assertDatabaseHas;
@@ -24,18 +22,15 @@ expect()->extend(
 );
 
 expect()->extend('toMatchQuery', function (string $sql, array $bindings, bool $exact = true) {
-    /** @var Builder|Relation $query */
-    $query = $this->value;
-
     if ($exact) {
-        Assert::assertSame($sql, $query->toSql());
-        Assert::assertSame($bindings, $query->getBindings());
+        Assert::assertSame($sql, $this->value->toSql());
+        Assert::assertSame($bindings, $this->value->getBindings());
     } else {
-        Assert::assertStringContainsString($sql, $query->toSql());
+        Assert::assertStringContainsString($sql, $this->value->toSql());
 
-        $bindingsCountBefore = Str::of($query->toSql())->before($sql)->substrCount('?');
+        $bindingsCountBefore = Str::of($this->value->toSql())->before($sql)->substrCount('?');
 
-        $queryBindings = $query->getBindings();
+        $queryBindings = $this->value->getBindings();
         $queryBindings = array_slice($queryBindings, $bindingsCountBefore, count($bindings));
 
         Assert::assertSame($bindings, $queryBindings);
