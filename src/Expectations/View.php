863015a9ec758c889528b96f001eb0d20e254261
Nicola <nicola_p92@hotmail.it>
2022-03-15 17:46:54 +0100

    Scritta proposta per nuova estensione di expect()

---
+++ src/Expectations/View.php
@@ -0,0 +1,20 @@
+<?php
+
+use Pest\Expectation;
+
+expect()->extend(
+    'toBeView',
+    /**
+     * Assert that the given view is an instance of view and name is identical
+     */
+    function (string $name): Expectation {
+        /** @var \Illuminate\View\View $view */
+        $view = $this->value;
+
+        expect($view)
+            ->toBeInstanceOf(\Illuminate\View\View::class)
+            ->name()->toBe($name);
+
+        return $this;
+    }
+);
