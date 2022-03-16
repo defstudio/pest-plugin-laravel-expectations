f304bccbbabc6526795e235d5ef3dbdb4b4a2131
Nicola <nicola_p92@hotmail.it>
2022-03-16 15:54:29 +0100

    Fix

--- src/Expectations/View.php
+++ src/Expectations/View.php
@@ -15,12 +15,8 @@ expect()->extend(
      * Assert that the given view is an instance of view and name is identical
      */
     function (string $name): Expectation {
-        /** @var \Illuminate\View\View $view */
-        $view = $this->value;
-
-        expect($view)
             // @phpstan-ignore-next-line
-            ->toBeInstanceOf(\Illuminate\View\View::class)
+            $this->toBeInstanceOf(\Illuminate\View\View::class)
             ->name()->toBe($name);
 
         return $this;
