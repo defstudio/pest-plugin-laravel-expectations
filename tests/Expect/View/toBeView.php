26624fedce10749ad8893a6908f4c50575600397
Nicola <nicola_p92@hotmail.it>
2022-03-16 16:42:49 +0100

    Test ToBeView

--- tests/Expect/View/toBeView.php
+++ tests/Expect/View/toBeView.php
@@ -1,5 +1,11 @@
 <?php
 
+use PHPUnit\Framework\ExpectationFailedException;
+
 test('pass', function () {
     expect(view('page'))->toBeView('page');
 });
+
+test('fail', function () {
+    expect('page')->toBeView('page');
+})->throws(ExpectationFailedException::class);
