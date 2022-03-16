f7ef48298f9170c5c4aa8ff35ddb90a3542f5d71
Nicola <nicola_p92@hotmail.it>
2022-03-16 16:28:13 +0100

    Test ToBeView

--- tests/Expect/View/toBeView.php
+++ tests/Expect/View/toBeView.php
@@ -1,6 +1,9 @@
 <?php
 
 use PHPUnit\Framework\ExpectationFailedException;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 
 test('pass', function () {
    expect(view('index'))->toBeView('index');
